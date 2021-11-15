0864a07c934635669bc224c46727a8767d2c1e84
lcr <llcoolryo@gmail.com>
2021-11-15 21:09:08 +0900

    Initial Commit

---
+++ YOLO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YOLO
+//
+//  Created by lcr on 2021/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YOLO/YOLOApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLOApp.swift
+//  YOLO
+//
+//  Created by lcr on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct YOLOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
